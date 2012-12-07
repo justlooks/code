2db9213d4c4667d33c48c0761ba81d0ff0f8e6ce
hch <justlooks@gmail.com>
2012-12-07 11:38:22 +0800

    添加oracle连接，连接释放函数 dbhandle_get,dbhandle_release

--- perl/test_oracle.pl
+++ perl/test_oracle.pl
@@ -1,12 +1,56 @@
 #!/usr/bin/perl 
 
+
 use DBI;
-
-#$ENV{ORACLE_HOME}="/opt/oracle/app/oracle/product/11.2.0/dbhome_1";
-#$ENV{ORACLE_SID}="mycatalog";
-$ENV{LD_LIBRARY_PATH}="/opt/oracle/app/oracle/product/11.2.0/dbhome_1/lib";
+use Data::Dumper;
 
 
-$dbh = DBI->connect("dbi:Oracle:mycatalog","hr","hr") or die("DB can not connect!");
+sub dbhandle_get {
+	my $info = shift;
+	my $handle = DBI->connect("dbi:Oracle:host=$info->{host};sid=$info->{sid}",$info->{user},$info->{pass},{RaiseError=>0,PrintError=>0,ora_session_mode=>2}) or die("connect refused ",$DBI::errstr,"\n");
+	return $handle;	
+}
 
-print "ok\n";
+sub dbhandle_release {
+	my $dbh = shift;
+	$dbh->disconnect or die $dbh->errstr;
+}
+
+sub info_fetch {
+	my ($sqlstr,$dbh) = @_;
+#	print "1 get $#$re\n";
+	my $sth = $dbh->prepare("$sqlstr");
+	$sth->execute();
+	my $hash_ref;
+#	if($re eq 1) {
+#		print "return struct\n";
+#		my $depref = $sth->fetchrow_arrayref;
+#		$sth->finish;
+#		return $depref;	
+#	}
+	while($hash_ref = $sth->fetchrow_hashref) {
+		print "i get sth\n";
+			print $hash_ref->{COLUMN_NAME},"\n";
+	}
+
+}
+
+my %conn_info = (
+	'user' => 'sys',
+	'pass' => '123456',
+	'host' => 'localhost',
+	'sid'  => 'mycatalog' );
+
+#print Dumper(\%conn_info);
+my $dbh = dbhandle_get(\%conn_info);
+
+#my $sql = q{desc v$instance};
+my $sql = q{select COLUMN_NAME from dba_tab_columns where table_name='T1' and owner='HR'};
+
+my $redep = 1;
+my $deparr_ref = info_fetch($sql,$dbh);
+
+dbhandle_release($dbh);
+
+print Dumper(@$deparr_ref);
+
