c3092f3864a12600693bfd70c05589903e06049d
hch <justlooks@gmail.com>
2012-12-07 19:22:17 +0800

    修改info_fetch调用，添加返回结果集格式化长度信息

--- perl/test_oracle.pl
+++ perl/test_oracle.pl
@@ -15,21 +15,29 @@ sub dbhandle_release {
 	$dbh->disconnect or die $dbh->errstr;
 }
 
-# return multi-array result set
+# return multi-array result set,返回结果集和格式化长度信息
 sub info_fetch {
 	my ($sqlstr,$dbh) = @_;
 	my $sth = $dbh->prepare("$sqlstr");
 	$sth->execute();
 	my $arr_ref;
 	my @rows = ();
+	my @format_len = ();
 	while($arr_ref = $sth->fetchrow_arrayref) {
 		my @arr_temp = ();
+		my $tmp_len;
 		for my $k (0..$#$arr_ref) {
 			push(@arr_temp,$arr_ref->[$k]);
+			$tmp_len = length("$arr_ref->[$k]");
+#			print "$tmp_len ---> $arr_ref->[$k] => ".length("$arr_ref->[$k]")."\t";
+			if($tmp_len > $format_len[$k]) {
+				$format_len[$k] = $tmp_len; 
+			}
 		}
+#		print "\n";
 		push(@rows,[@arr_temp]);	
 	}
-	return \@rows;
+	return (\@rows,\@format_len);
 }
 
 my %conn_info = (
@@ -44,12 +52,13 @@ my $dbh = dbhandle_get(\%conn_info);
 my $sql = q{select space_usage_kbytes,occupant_name,occupant_desc from v$sysaux_occupants order by 1 desc};
 #my $sql = q{select COLUMN_NAME from dba_tab_columns where table_name='T1' and owner='HR'};
 
-my $re_ref = info_fetch($sql,$dbh);
-print ${$re_ref->[1]}[1],"\n";
+my ($re_ref,$fmt_arr) = info_fetch($sql,$dbh);
+for my $k (0..$#$fmt_arr) {
+	print $fmt_arr->[$k],"\t";
+}
+print "\n";
 
 dbhandle_release($dbh);
 
 
-#my $test_ref = [ [ 'a', 'b', 'c' ],[ '2', '3', '4' ] ];
-#print ${$test_ref->[1]}[0],"\n";
 
