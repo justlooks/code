a6fcfbed2e4005404508d719eef2c391cd03e3f0
hch <justlooks@gmail.com>
2012-12-08 10:50:08 +0800

    添加result_print调用，传入参数为结果集，结果集格式参数，title参数

--- perl/test_oracle.pl
+++ perl/test_oracle.pl
@@ -29,15 +29,35 @@ sub info_fetch {
 		for my $k (0..$#$arr_ref) {
 			push(@arr_temp,$arr_ref->[$k]);
 			$tmp_len = length("$arr_ref->[$k]");
-#			print "$tmp_len ---> $arr_ref->[$k] => ".length("$arr_ref->[$k]")."\t";
 			if($tmp_len > $format_len[$k]) {
 				$format_len[$k] = $tmp_len; 
 			}
 		}
-#		print "\n";
 		push(@rows,[@arr_temp]);	
 	}
 	return (\@rows,\@format_len);
+}
+
+# format output
+
+sub result_print {
+	my ($re,$fmt,$title) = @_;
+	my $len;
+	for my $k (0..$#$fmt) {
+		if($fmt->[$k] < length("$title->[$k]")) {
+			$fmt->[$k] = length("$title->[$k]");
+		}
+		$len += $fmt->[$k] + 4;
+		printf "%-$fmt->[$k]s\t",$title->[$k];
+	}
+	$len += 4;
+	printf "\n" . "-" x $len ."\n";
+	for my $i (0..$#$re) {
+		for my $j (0..$#{$re->[$i]}) {
+			printf "%-$fmt->[$j]s\t",${$re->[$i]}[$j]; 
+		}
+		printf "\n";
+	}
 }
 
 my %conn_info = (
@@ -52,11 +72,14 @@ my $dbh = dbhandle_get(\%conn_info);
 my $sql = q{select space_usage_kbytes,occupant_name,occupant_desc from v$sysaux_occupants order by 1 desc};
 #my $sql = q{select COLUMN_NAME from dba_tab_columns where table_name='T1' and owner='HR'};
 
+my $head = [uc('space_usage_kbytes'),uc('occupant_name'),uc('occupant_desc')];
+#my $head = ['COLUMN_NAME'];
+
 my ($re_ref,$fmt_arr) = info_fetch($sql,$dbh);
-for my $k (0..$#$fmt_arr) {
-	print $fmt_arr->[$k],"\t";
-}
-print "\n";
+
+#print "<--- ${$re_ref->[1]}[1]\n";
+
+result_print($re_ref,$fmt_arr,$head);
 
 dbhandle_release($dbh);
 
